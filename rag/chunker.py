51e007b83aa9f295d3092134d609bbe7a2f8c6e6
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-08-01 20:46:16 +0530

    Update chunker.py

--- rag/chunker.py
+++ rag/chunker.py
@@ -2,19 +2,14 @@
 Document Chunking
 """
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def split_documents(documents):
 
     splitter = RecursiveCharacterTextSplitter(
-
         chunk_size=1000,
-
         chunk_overlap=200
-
     )
 
-    chunks = splitter.split_documents(documents)
-
-    return chunks
+    return splitter.split_documents(documents)
