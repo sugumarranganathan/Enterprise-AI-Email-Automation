a540826624af06b9586062323a441c3ed154e7d5
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-08-01 21:03:31 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,28 +1,21 @@
 """
 Application Configuration
 
-Works in:
-
-✅ Google Colab
-✅ Local Linux
-✅ Render
+Supports:
+- Google Colab Secrets
+- .env (Local)
+- Environment Variables (Render)
 """
 
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# ----------------------------------------------------
 # Detect Google Colab
-# ----------------------------------------------------
-
 try:
     from google.colab import userdata
-
     IS_COLAB = True
-
 except ImportError:
     IS_COLAB = False
 
@@ -30,19 +23,12 @@ except ImportError:
 class Settings:
 
     if IS_COLAB:
-
         GROQ_API_KEY = userdata.get("GROQ_API_KEY")
-
         QDRANT_URL = userdata.get("QDRANT_URL")
-
         QDRANT_API_KEY = userdata.get("QDRANT_API_KEY")
-
     else:
-
         GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
         QDRANT_URL = os.getenv("QDRANT_URL")
-
         QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
 
     QDRANT_COLLECTION = os.getenv(
