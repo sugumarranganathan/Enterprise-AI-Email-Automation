cc90e27bcf5a732122ff5e18f5f4322a351a35d2
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-08-02 15:18:33 +0530

    Update retriever.py

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,8 +1,14 @@
 """
 Retriever
+
+Retrieves relevant documents from the Qdrant vector database.
 """
 
 from rag.vectorstore import vectorstore
+from utils.logger import logger
+
+
+logger.info("Initializing Retriever...")
 
 
 retriever = vectorstore.as_retriever(
@@ -16,6 +22,46 @@ retriever = vectorstore.as_retriever(
 )
 
 
-def retrieve(question):
+def retrieve(question: str):
 
-    return retriever.invoke(question)
+    """
+    Retrieve relevant documents.
+
+    Parameters
+    ----------
+    question : str
+        User query.
+
+    Returns
+    -------
+    list
+        List of relevant LangChain Document objects.
+    """
+
+    logger.info("=" * 60)
+    logger.info("===== Retriever Started =====")
+    logger.info("=" * 60)
+
+    try:
+
+        logger.info(f"Query : {question}")
+
+        documents = retriever.invoke(question)
+
+        logger.info(f"Retrieved {len(documents)} document(s).")
+
+        logger.info("=" * 60)
+        logger.info("===== Retriever Completed =====")
+        logger.info("=" * 60)
+
+        return documents
+
+    except Exception as e:
+
+        logger.exception("Retriever failed.")
+
+        logger.info("=" * 60)
+        logger.info("===== Retriever Completed =====")
+        logger.info("=" * 60)
+
+        return []
