75e717d39fa57733470a2a0e7dfa83e167a3b912
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-08-03 10:47:59 +0530

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -5,12 +5,12 @@ Tools Package
 ====================================================
 """
 
-from .gmail_reader import read_emails
+from .gmail_reader import read_latest_email
 from .gmail_sender import send_email
 from .qdrant_tool import client
 
 __all__ = [
-    "read_emails",
+    "read_latest_email",
     "send_email",
     "client",
 ]
